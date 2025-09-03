65dd905dd1989c1505c713da693c6a47f92ac66c
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-04 00:01:32 +0300

    modified commit

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ import sys
 
 def main():
     name = input("What is your name?")
-    print(f"Hello{name}!")
+    age = int(input("what is your age?"))
+    print(f"Hello{name}! In {2022 + (120 - age)}you will be 120 years old . Tough luck!")
     return 0
 
 if __name__ == "__main__":
