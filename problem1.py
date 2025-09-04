bd856ad4d702ea3db15dd9ec5279bc164684333a
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-04 13:18:08 +0300

    problem1.py

--- problem1.py
+++ problem1.py
@@ -1,6 +1,21 @@
-#d_squared = (x1-x2)^2 + (y1-y2)^2
+#d_squared = ...
 
 import sys
 def main():
 
-    # A
+ # a = (x1, y1, z1)
+   x1 = float(input("x1: "))
+   y1 = float(input("y1: "))
+   z1 = float(input("z1:"))
+  # b = ( x2, y2, z2)
+   x2 = float(input("x2: "))
+   y2 = float(input("y2:"))
+   z2 = float(input("z2: "))
+   print(type(x1))
+   d_squared = (x1 - x2) **2 + (y1 - y2) **2 + (z1 - z2) ** 2
+   print(f"{d_squared = }")
+   return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
