eb78ee530a01d7c6fda7062e23a9b1cf86d97756
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-08 23:00:55 +0300

    divisors.py

--- divisors.py
+++ divisors.py
@@ -1,18 +1,30 @@
 import sys
 
-def main():
-    joint_divisors = list()
-    other = list()
-    for number in range(100):
-        if number % 2 == 0 and number % 11 == 0:
-            joint_divisors.append(number)
-        else:
-            other.append(number)
+def get_joint_divisors(divisor1, divisor2, maximum):
+   joint_divisors = []
+   other = []
 
-    print(f"{joint_divisors = }")
-    print()
-    print(f"{other = }")
-    return 0
+   for number in range(maximum +1):
+       if number % divisor1 == 0 and number % divisor2 == 0:
+           joint_divisors.append(number)
+       else:
+           other.append(number)
+
+   return joint_divisors,other
+
+def main():
+  divisor1 = 2
+  divisor2 = 12
+  maximum =  50
+
+  joint_divisors, other = get_joint_divisors(divisor1, divisor2, maximum)
+
+  print(f"{joint_divisors = }")
+  print()
+  print(f"{other = }")
+
+  return 0
+
 
 if __name__ == "__main__":
     sys.exit(main())
