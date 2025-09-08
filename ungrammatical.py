93d16d124d588c279068597cdbda4136c88cba93
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-08 23:01:30 +0300

    ungrammatical.py

---
+++ ungrammatical.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    name = input("Enter your name:")
+    if name.istitle():
+     print(f"Ungrammatical: write'   ")
+
+
+    return 0
+
+    if __name__ == "__main__":
+        sys.exit(main())
