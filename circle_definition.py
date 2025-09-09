22865d93298747a286db86567505d39f4acf40ad
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-09 14:53:35 +0300

    circle_definition.py

--- circle_definition.py
+++ circle_definition.py
@@ -2,9 +2,11 @@ import sys
 class Circle:
     units = 'cm'
 
-    def __init__(self, radius, position=(0, 0 )):
+    def __init__(self, radius, position=(0, 0 ),):
         self.radius = radius
         self.position = position
+        self.diameter = 2 * radius
+        self.area = 3.142 * radius**2
 
     def __str__(self):
         return f"I am a circle of radius {self.radius}{self.units}located at{self.position}"
@@ -16,7 +18,9 @@ def main():
     print(circle)
     circle.radius = 12
     print(circle)
-
+    print(str(circle))
+    print(f"My diameter is {circle.diameter}")
+    print(f"My area is {circle.area}")
 
     return 0
 
