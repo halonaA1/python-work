8834deb70f916dd2bf120895d0a7a35c0ab9c96c
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-09 22:26:52 +0300

    draw_rectangle.py

--- draw_rectangle.py
+++ draw_rectangle.py
@@ -11,8 +11,24 @@ def draw_rectangle():
     turtle.forward(110)
     turtle.done()
 
+def draw_circle():
+    pen = turtle.Turtle()
+    if pen.isdown():
+        pen.up()
+    pen.goto(12, 20)
+    pen.down()
+    pen.begin_fill()
+    pen.pencolor("blue")
+    pen.fillcolor("purple")
+    pen.circle(60)
+    pen.end_fill()
+    pen.up()
+    turtle.done()
+
 def main():
     draw_rectangle()
+
+    draw_circle()
     return 0
 if __name__ == '__main__':
     sys.exit(main())
