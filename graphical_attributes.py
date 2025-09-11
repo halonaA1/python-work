6c6720bda41638f8acda063c1ab158fdd57dc6dd
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-11 20:37:14 +0300

    graphical_attributes.py

---
+++ graphical_attributes.py
@@ -0,0 +1,93 @@
+import sys
+import math
+
+
+class Circle:
+    units = 'cm'
+
+    def __init__(self, radius, fill="yellow", stroke="black", position=(0, 0)):
+        self.radius = radius
+        self.position = position
+        self.fill = fill
+        self.stroke = stroke
+
+        self.diameter = radius * 2
+
+    def __str__(self):
+        return f"circle of radius {self.radius} {self.units} located at {self.position}."
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 *math.pi * self.radius
+
+    def arc_length(self, theta, radians=True):
+        if radians:
+            return theta * self.radius
+        else:
+            theta_radians = math.radians(theta)
+            return theta_radians * self.radius
+
+    def bounding_box(self):
+        x_min = self.position[0] - self.radius
+        x_max = self.position[0] + self.radius
+        y_min = self.position[1] - self.radius
+        y_max = self.position[1] + self.radius
+        return x_min, y_min, x_max, y_max
+
+
+class Rectangle:
+    units = 'cm'
+
+    def __init__(self, width, length, fill="green", stroke="black", position=(0, 0)):
+        self.width = width
+        self.length = length
+        self.position = position
+        self.fill = fill
+        self.stroke = stroke
+    def __str__(self):
+        return f"rectangle of width {self.width} {self.units} located at {self.position}."
+
+    def area(self):
+        return self.width * self.length
+
+    def perimeter(self):
+        return (2 * self.width) + (2 * self.length)
+
+    def diagonal(self):
+        return math.sqrt(self.width ** 2 + self.length ** 2)
+
+    def bounding_box(self):
+        x_min = self.position[0] - self.width / 2
+        x_max = self.position[0] + self.width / 2
+        y_min = self.position[1] - self.length / 2
+        y_max = self.position[1] + self.length / 2
+        return x_min, y_min, x_max, y_max
+
+
+def main():
+    circle = Circle(radius=4)
+    print(circle)
+    circle.position = (12, 15)
+    print(circle)
+    print(f"Circle diameter: {circle.diameter} {circle.units} ")
+    print(f"Circle area = {circle.area()} {circle.units}^2")
+    print(f"Circle perimeter = {circle.perimeter()} {circle.units}")
+    print(f"Circle arc length = {circle.arc_length(0.5)} ")
+    print(f" {circle.bounding_box() = }")
+
+    rectangle = Rectangle(100, 250)
+    print(rectangle)
+    rectangle.position = (25, 40)
+    print(rectangle)
+    print(f"Rectangle area = {rectangle.area()} {rectangle.units}^2")
+    print(f"Rectangle perimeter = {rectangle.perimeter()} {rectangle.units}")
+    print(f"Rectangle diagonal = {rectangle.diagonal()} {rectangle.units}")
+    print(f"{rectangle.bounding_box()} = ")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
