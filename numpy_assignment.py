1c4f34d55b7cc5325a916cc39cc67abb7360bbe5
Halona <hhaomondi@students.uonbi.ac.ke>
2025-09-21 16:38:14 +0300

    numpy_assignment.py

--- numpy_assignment.py
+++ numpy_assignment.py
@@ -17,7 +17,7 @@ class TextShape(Shape):
         self.position = position
 
     def draw(self, canvas):
-        canvas.create_text(self.position[0], self.position[1], text=self.text, font = ("Ariel", 14), fill= "brown")
+        canvas.create_text(self.position[0], self.position[1], text=self.text, font = ("Times New Roman", 14), fill= "brown")
 def main():
     A = np.array([[1, 2], [3, 4]])
     B = np.array([[5, 6], [7, 8]])
@@ -29,33 +29,27 @@ def main():
     norm_A = math.sqrt(np.sum( A ** 2))
     norm_B = math.sqrt(np.sum( B ** 2))
 
-    root = tk.Tk()
-    root.title("Numpy + Math + Canvas + Text")
-    canvas = tk.Canvas(root, width=800, height=600, bg="white")
-    canvas.pack()
+    print("Matrix A:\n", A)
+    print("Matrix B:\n", B)
+    print("\nA + B =\n", add_result)
+    print("\nA * B (elementwise) =\n", mult_result)
+    print("\nA @ B (matrix product) =\n", matmul_result)
+    print(f"\n||A|| = {norm_A: .2f}")
+    print(f"||B|| = {norm_B: .2f}")
 
-    t1 = TextShape("Addition", f"A + B =\n {add_result}", position=(200, 100))
-    t2 = TextShape("Elementwise Multiplication", f"A *B =\n {mult_result}", position=(200, 150))
-    t3 = TextShape("Matrix Product", f"A @ B = \n {matmul_result}", position=(200, 200))
-    t4 = TextShape("Norm", f"||A|| = {norm_A}", position=(200, 300))
-    t5 = TextShape("Norm", f"||B|| = {norm_B}", position=(200, 400))
+    screen = turtle.Screen()
+    screen.title("Turtle Window")
+    pen = turtle.Turtle()
+    pen.color("purple")
+    pen.pensize(2)
 
-    for t in [t1, t2, t3, t4, t5]:
-        t.draw(canvas)
+    for _ in range(4):
+        pen.forward(100)
+        pen.left(90)
+        pen.forward(100)
 
-        screen = turtle.Screen()
-        screen.title("Turtle Window")
-        pen = turtle.Turtle()
-        pen.color("Black")
-        pen.pensize(3)
-
-        for _ in range(3):
-            pen.forward(100)
-            pen.left(90)
-
-        root.mainloop()
-
-        return 0
+    turtle.done()
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
